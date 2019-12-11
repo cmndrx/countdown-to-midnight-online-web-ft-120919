37d213e837d2e322502004bb545d2885def80e71
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-11 18:57:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 counter = 10
 
-def countdown(counter)
+def countdown
   while counter > 0
   counter -= 1
   puts "#{counter} SECOND(S)!"
   end
   
-  while counter == 0
+  
   puts "HAPPY NEW YEAR!"
-  end
+  
   
 end
